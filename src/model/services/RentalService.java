da451a6fe838325b05ff42028744f4ed28106917
Leonardo Gregorio <leonard.gregorio@gmail.com>
2018-07-10 18:04:31 -0300

    projeto sem interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,44 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerhour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerhour, BrazilTaxService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerhour = pricePerhour;
+		this.taxService = taxService;
+	}
+	
+	public RentalService () {
+		
+	}
+	
+	public void processingInvoice(CarRental carRental) {
+		
+		long t1 =carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2-t1) /1000/60/60;
+		
+		double basicPayment;
+		if(hours <=12.0) {
+			basicPayment = Math.ceil(hours)*pricePerhour;
+		}
+		else {
+			basicPayment = Math.ceil(hours /24)* pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment,tax));
+		
+		
+	}
+}
